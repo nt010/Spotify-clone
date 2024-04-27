e2824819dd66520449950a4ea2179def3e1ba68a
nt <tomnkmr0120@gmail.com>
2024-04-27 12:26:35 +0900

    manage by class

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -1,18 +1,31 @@
 import axios from "axios"
 
-export const getToken = async () => {
-    const response = await axios.post(
-        "https://accounts.spotify.com/api/token",
-        {
-          grant_type: "client_credentials",
-          client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
-          client_secret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
-        },
-        {
-          headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          }
-        }
-    );
-    console.log(response.data);
+class SpotifyClient {
+    static async initialize() {
+        const response = await axios.post(
+            "https://accounts.spotify.com/api/token",
+            {
+              grant_type: "client_credentials",
+              client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+              client_secret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
+            },
+            {
+              headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              }
+            }
+        );
+
+        // access_tokenがセットされたインスタンスを返す
+        let spotify = new SpotifyClient();
+        spotify.token = response.data.access_token;
+        return spotify;
+    }
+
+    test() {
+        console.log(this.token);
+    }
 }
+
+const spotify = await SpotifyClient.initialize();
+export default spotify;
