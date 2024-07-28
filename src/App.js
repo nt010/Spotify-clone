2cf0c04130f0cd0ad4d5e076e3000b23217b9727
nt <tomnkmr0120@gmail.com>
2024-07-29 00:54:21 +0900

    add function of search

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import { useEffect, useRef, useState } from "react";
 import { SongList } from "./components/SongList";
 import spotify from "./lib/spotify";
 import { Player } from "./components/Player";
+import { SearchInput } from "./components/SearchInput";
+import { Pagination } from "./components/Pagination";
+
+const limit = 20;
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [popularSongs, setPopularSongs] = useState([]);
   const [isPlay, setIsPlay] = useState(false);
   const [selectedSong, setSelectedSong] = useState();
+  const [keyword, setKeyword] = useState("");
+  const [searchedSongs, setSearchedSongs] = useState(null);
+  const [page, setPage] = useState(1);
+  const [hasNext, setHasNext] = useState(false);
+  const [hasPrev, setHasPrev] = useState(false);
   const audioRef = useRef(null);
+  const isSearchedResult = searchedSongs != null;
 
   useEffect(() => {
     fetchPopularSongs();
@@ -52,19 +62,56 @@ export default function App() {
     }
   };
 
+  const handleInputChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const searchSongs = async (page) => {
+    setIsLoading(true);
+    const offset = parseInt(page) ? (parseInt(page) - 1) * limit : 0;
+    const result = await spotify.searchSongs(keyword, limit, offset);
+    setHasNext(result.next != null);
+    setHasPrev(result.previous != null);
+
+    console.log("AAAAAAA", result);
+    setSearchedSongs(result.items);
+    setIsLoading(false);
+  };
+
+  const moveToNext = async () => {
+    const nextPage = page + 1;
+    await searchSongs(nextPage);
+    setPage(nextPage);
+  };
+
+  const moveToPrev = async () => {
+    const prevPage = page + 1;
+    await searchSongs(prevPage);
+    setPage(prevPage);
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       <main className="flex-1 p-8 mb-20">
         <header className="flex justify-between items-center mb-10">
           <h1 className="text-4xl font-bold">Music App</h1>
         </header>
+        <SearchInput onInputChange={handleInputChange} onSubmit={searchSongs} />
         <section>
-          <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
+          <h2 className="text-2xl font-semibold mb-5">
+            {isSearchedResult ? "Searched Result" : "Popular Songs"}
+          </h2>
           <SongList
             isLoading={isLoading}
-            songs={popularSongs}
+            songs={isSearchedResult ? searchedSongs : popularSongs}
             onSongSelected={handkeSongSelected}
           />
+          {isSearchedResult && (
+            <Pagination
+              onPrev={hasPrev ? moveToPrev : null}
+              onNext={hasNext ? moveToNext : null}
+            />
+          )}
         </section>
       </main>
       {selectedSong != null && (
